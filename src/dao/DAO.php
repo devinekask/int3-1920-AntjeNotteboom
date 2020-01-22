904e8e0c93f8c23fad6475d514aac2d990211b5e
antje notteboom <antje.notteboom@student.howest.be>
2020-01-22 12:48:21 +0100

    dao change

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -7,22 +7,22 @@ class DAO {
   // protected $pdo;
 
   // no wifi
-  // private static $dbHost = "localhost";
-  // private static $dbName = "humo";
-  // private static $dbUser = "humo";
-  // private static $dbPass = "humo123";
-  // private static $sharedPDO;
-  // protected $pdo;
+  private static $dbHost = "localhost";
+  private static $dbName = "humo";
+  private static $dbUser = "humo";
+  private static $dbPass = "humo123";
+  private static $sharedPDO;
+  protected $pdo;
 
 
   // Constructor
   function __construct() {
 
     if(empty(self::$sharedPDO)) {
-      $dbHost = getenv('PHP_DB_HOST') ?: " localhost";
-      $dbName = getenv('PHP_DB_DATABASE') ?: "humo";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "humo";
-      $dbPass = getenv('PHP_DB_PASSWORD') ?: "humo123";
+      // $dbHost = getenv('PHP_DB_HOST') ?: " localhost";
+      // $dbName = getenv('PHP_DB_DATABASE') ?: "humo";
+      // $dbUser = getenv('PHP_DB_USERNAME') ?: "humo";
+      // $dbPass = getenv('PHP_DB_PASSWORD') ?: "humo123";
 
       self::$sharedPDO = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
       self::$sharedPDO->exec("SET CHARACTER SET utf8");
