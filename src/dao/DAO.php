3c123dc425ebb06708a28661b7f198970729c48b
antje notteboom <antje.notteboom@student.howest.be>
2020-01-23 13:28:04 +0100

    update

--- src/dao/DAO.php
+++ src/dao/DAO.php
@@ -19,10 +19,10 @@ class DAO {
   function __construct() {
 
     if(empty(self::$sharedPDO)) {
-      $dbHost = getenv('PHP_DB_HOST') ?: " localhost";
-      $dbName = getenv('PHP_DB_DATABASE') ?: "humo";
-      $dbUser = getenv('PHP_DB_USERNAME') ?: "humo";
-      $dbPass = getenv('PHP_DB_PASSWORD') ?: "humo123";
+      $dbHost = getenv('PHP_DB_HOST') ?: "ID281988_humo.db.webhosting.be";
+      $dbName = getenv('PHP_DB_DATABASE') ?: "ID281988_humo";
+      $dbUser = getenv('PHP_DB_USERNAME') ?: "ID281988_humo";
+      $dbPass = getenv('PHP_DB_PASSWORD') ?: "humo1234";
 
       self::$sharedPDO = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $dbPass);
       self::$sharedPDO->exec("SET CHARACTER SET utf8");
